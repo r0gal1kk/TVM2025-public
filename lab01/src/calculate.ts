9c4dfcf241ef2aa33a7f451f9cfbef95239c9636
n.bliznyuk <n.bliznyuk@g.nsu.ru>
2025-09-29 02:28:43 +0700

    sum realization

--- lab01/src/calculate.ts
+++ lab01/src/calculate.ts
@@ -5,7 +5,20 @@ export const addMulSemantics: AddMulSemantics = grammar.createSemantics() as Add
 
 
 const addMulCalc = {
-/// write the action rules here
+    Expr(expr) {
+      return expr.calculate();
+    },
+    AddExpr(firstNumber, pluses, otherNumbers) {
+      let sum = firstNumber.calculate();
+      for (let i = 0; i < pluses.numChildren; i++) {
+          let nextNumber = otherNumbers.child(i);
+          sum += nextNumber.calculate();
+      }
+      return sum;
+    },
+    number(chars) {
+        return parseInt(this.sourceString);
+    }
 } satisfies AddMulActionDict<number>
 
 addMulSemantics.addOperation<Number>("calculate()", addMulCalc);
--- lab01/src/index.ts
+++ lab01/src/index.ts
@@ -12,10 +12,17 @@ export class SyntaxError extends Error
 
 function parse(content: string): MatchResult
 {
-    throw "Not implemented";
+    const match = grammar.match(content);
+    if (match.failed()) {
+        throw new SyntaxError(match.message || "Syntax error");
+    }
+    return match;
 }
 
 function calculate(expression: MatchResult):number
 {
-    throw "Not implemented"
+    return addMulSemantics(expression).calculate();
 }
+
+
+console.log(evaluate("1+2+3+4"));

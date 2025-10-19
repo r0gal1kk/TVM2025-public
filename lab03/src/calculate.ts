42a3f6ecde7610856f9ca37c6e78283b906cfa1e
n.bliznyuk <n.bliznyuk@g.nsu.ru>
2025-10-19 17:23:15 +0700

    lab3 final

--- lab03/src/calculate.ts
+++ lab03/src/calculate.ts
@@ -1,4 +1,4 @@
-import {MatchResult} from "ohm-js";
+import {MatchResult, Node, NonterminalNode} from "ohm-js";
 import grammar, {ArithmeticActionDict, ArithmeticSemantics} from "./arith.ohm-bundle";
 
 export const arithSemantics: ArithSemantics = grammar.createSemantics() as ArithSemantics;
@@ -8,19 +8,22 @@ const arithCalc = {
         return Number(numNode.sourceString);
     },
 
-    number_variable(varNode: any): number {
+    number_variable(varNode: Node): number {
         const varName = varNode.sourceString;
         const value = this.args.params[varName];
         return value !== undefined ? value : NaN;
     },
 
-    Sum(sumNode: any): number {
-        const children = sumNode.asIteration().children;
-        let total = 0;
-        for (const child of children) {
-            total = total + child.calculate(this.args.params);
-        }
-        return total;
+    Sum(sumNode: NonterminalNode): number {
+        return sumNode.asIteration().children.reduce(
+            (acc, c) => acc + c.calculate(this.args.params),
+            0);
+        //
+        // let total = 0;
+        // for (const child of children) {
+        //     total = total + child.calculate(this.args.params);
+        // }
+        // return total;
     },
 
     Mul(mulNode: any): number {
